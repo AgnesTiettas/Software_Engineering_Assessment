f04668d7fe0b4130b9cd7d54a529509e797f8140
AgnesTiettas <agnes.tiettas1@education.nsw.gov.au>
2024-05-14 23:05:19 +1000

    Version 4
    Continued to work on the first page of the code and made sure that the program has a fixed size.

--- Primary_and_Secondary_Colours.py
+++ Primary_and_Secondary_Colours.py
@@ -1,11 +1,12 @@
 from tkinter import * 
 root=Tk() 
 root.title("Simple Colour Wheel")
-root.geometry("400x400")
+root.geometry("400x400") 
+root.resizable(False,False) #Make sure that the page maintains its size at 400 by 400. 
 
 Label1=Label(root,text="Primary and Secondary Colours", 
              font=("Arial",20),
-             bg="lightgrey", 
+             bg= "#d5bdaf", 
              fg="black",
              padx=20,
              pady=10,
@@ -17,20 +18,29 @@ Label2=Label(root,text="Let's Learn about Primary and Secondary Colours!",
              font=("Arial",12), 
              fg="black",
              padx=0,
-             pady=40,
+             pady=60,
              relief="raised",
              borderwidth=0,) #Creating a label widget 
 Label2.pack()
 
+button1=Button(root,text="START",
+               pady=20, 
+               padx=80,)
+button1.pack() 
 
 Label3=Label(root,text="Click to begin", 
              font=("Arial",10), 
              fg="black",
              padx=20,
-             pady=30,
+             pady=100,
              relief="raised",
              borderwidth=0,) #Creating a label widget 
-Label3.pack()
+Label3.pack() 
+
+
+
+
+
 
 
 

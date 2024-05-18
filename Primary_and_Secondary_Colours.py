03c35970ff5b5ab66f6c47f8fdb12f7826d50651
AgnesTiettas <agnes.tiettas1@education.nsw.gov.au>
2024-05-19 00:00:21 +1000

    Version 5
    Continued to work on the first page and installed the PIL file into the code in order to be able to use images.

--- Primary_and_Secondary_Colours.py
+++ Primary_and_Secondary_Colours.py
@@ -1,11 +1,12 @@
 from tkinter import * 
+from PIL import Image,ImageTk
 root=Tk() 
 root.title("Simple Colour Wheel")
 root.geometry("400x400") 
 root.resizable(False,False) #Make sure that the page maintains its size at 400 by 400. 
 
 Label1=Label(root,text="Primary and Secondary Colours", 
-             font=("Arial",20),
+             font=("Times",20),
              bg= "#d5bdaf", 
              fg="black",
              padx=20,
@@ -15,7 +16,7 @@ Label1=Label(root,text="Primary and Secondary Colours",
 Label1.pack() 
 
 Label2=Label(root,text="Let's Learn about Primary and Secondary Colours!", 
-             font=("Arial",12), 
+             font=("Times",12), 
              fg="black",
              padx=0,
              pady=60,
@@ -23,10 +24,13 @@ Label2=Label(root,text="Let's Learn about Primary and Secondary Colours!",
              borderwidth=0,) #Creating a label widget 
 Label2.pack()
 
+
 button1=Button(root,text="START",
                pady=20, 
-               padx=80,)
+               padx=80,
+               bg="#ede0d4")
 button1.pack() 
+
 
 Label3=Label(root,text="Click to begin", 
              font=("Arial",10), 
@@ -36,14 +40,4 @@ Label3=Label(root,text="Click to begin",
              relief="raised",
              borderwidth=0,) #Creating a label widget 
 Label3.pack() 
-
-
-
-
-
-
-
-
-
-
 root.mainloop()
